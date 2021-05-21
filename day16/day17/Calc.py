fc8dd0d68ffc07f6817fed1108cd5a3eb2c4138c
yhy9527 <82857693+yhy9527@users.noreply.github.com>
2021-05-21 19:51:00 +0800

    Add files via upload

---
+++ day16/day17/Calc.py
@@ -0,0 +1,13 @@
+class  Calc:
+    # 加法
+    def  add(self,a,b):
+        return a + b
+
+    def reduce(self,a,b):
+        return a - b
+
+    def multi(self,a,b):
+        return a * b
+
+    def division(self,a,b):
+        return a // b
